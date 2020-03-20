981b866123aa7c18bd13844ecf941c4653afc6fa
Villespappa <54894773+Villespappa@users.noreply.github.com>
2020-03-20 10:47:54 +0100

    Add files via upload
    calling server functions instead of locally

--- menu.py
+++ menu.py
@@ -2,19 +2,12 @@ import sys #this allows you to use the sys.exit command to quit/logout of the ap
 import glob
 import os
 import requests
+import server
+from server import list_files, add_file, get_file, delete_file
+
 
 def main():
-##    login()
-##    
-##def login():
-##    username="kjell"
-##    password="master"
-##    print("Enter username : ")
-##    answer1=input()
-##    print("Enter password : ")
-##    answer2=input()
-##    if answer1==username and answer2==password:
-##        print("Welcome - Access Granted")
+
         menu()
 
 def menu():
@@ -32,13 +25,13 @@ def menu():
         Please enter your choice: """)
 
     if choice == "1":
-        listfiles()
+        server.list_files()
     elif choice == "2":
-        enterfilename()
+        server.add_file()
     elif choice == "3":
-        downloadfile()
+        server.get_file()
     elif choice == "4":
-        deletefile()
+        server.delete_file()
     elif choice=="Q" or choice=="q":
         sys.exit
     else:
@@ -46,66 +39,60 @@ def menu():
         print("Please try again")
         menu()
 
-def enterfilename():
-    # asks user for filename
-    filename = input("Insert 'filename.txt' >>> ")
-    # opens user inputted filename ".txt" and (w+) creates new file if it dont already exists
-    with open(filename, 'w+') as f:
-        f.close()
-        print("File created")
-
-##    http.request(
-##    'POST',
-##    'localhost:5000',
-##    )
-        menu()
-
-
-def listfiles():
-    myFiles = glob.glob('*.txt')
-    print(myFiles)
-    print()
-##    response = requests.get('localhost:5000')
-##    print(response)
-    pass        
-    menu()
-        
-
-def downloadfile():
-    myFiles = glob.glob('*.txt')
-    print(myFiles)
-    print()
-    filename = input("Enter name of file you want to download: >>> ")
-      # checking whether file exists or not
-    if  os.path.exists(filename):
-        f=open(filename, 'w+')
-        f.close()
-        print("File downloaded")
-    else:
-    # file not found message
-        print("File not found in the directory")
-    pass
-    
-
-
-
-def deletefile():
-    myFiles = glob.glob('*.txt')
-    print()
-   # print("Enter name of file you want to delete")
-    print(myFiles)
-    filename = input("Enter name of file you want to delete: ")
-    # checking whether file exists or not
-    if  os.path.exists(filename):
-        os.remove(filename)
-        print("File deleted")
-    else:
-    # file not found message
-        print("File not found in the directory")
-    
-    menu()    
-    
-
-menu()
+##def enterfilename():
+##    # asks user for filename
+##    filename = input("Insert 'filename.txt' >>> ")
+##    # opens user inputted filename ".txt" and (w+) creates new file if it dont already exists
+##    with open(filename, 'w+') as f:
+##        f.close()
+##        print("File created")
+##        menu()
+##
+##
+##def listfiles():
+##    myFiles = glob.glob('*.txt')
+##    print(myFiles)
+##    print()
+##
+##    pass        
+##    menu()
+##        
+##
+##def downloadfile():
+##    myFiles = glob.glob('*.txt')
+##    print(myFiles)
+##    print()
+##    filename = input("Enter name of file you want to download: >>> ")
+##      # checking whether file exists or not
+##    if  os.path.exists(filename):
+##        f=open(filename, 'w+')
+##        f.close()
+##        print("File downloaded")
+##    else:
+##    # file not found message
+##        print("File not found in the directory")
+##    pass
+##    
+##
+##
+##
+##def deletefile():
+##    myFiles = glob.glob('*.txt')
+##    print()
+##   # print("Enter name of file you want to delete")
+##    print(myFiles)
+##    filename = input("Enter name of file you want to delete: ")
+##    # checking whether file exists or not
+##    if  os.path.exists(filename):
+##        os.remove(filename)
+##        print("File deleted")
+##    else:
+##    # file not found message
+##        print("File not found in the directory")
+##    
+##    menu()    
+##    
+##
+##menu()
 
 main()
