e5d7d33d91a3fb22eb4e797057cfef248ae15b1d
MrSteffe <stefan_edenholm@hotmail.com>
2020-03-22 16:52:17 +0100

    Update testingUnittestServer.py

--- testingUnittestServer.py
+++ testingUnittestServer.py
@@ -3,24 +3,36 @@ import server
 
 class TestServerMethods(unittest.TestCase):
 
+    # This test should work, because the "PUT"-command replace the old file with this file,
+    # even if the files have the same name.
     def test_add_file1(self):
         file_name = "file_on_server.txt"
         output =  server.add_file(file_name)
 
         self.assertEqual(output, ("", 201))
 
+        
     def test_add_file2(self):
+        file_name = "not_a_file_on_server.txt"
+        output =  server.add_file(file_name)
+
+        self.assertEqual(output, ("", 201))
+
+        
+    def test_add_file3(self):
         file_name = "file/document/not_a_file_on_server.txt"
         output =  server.add_file(file_name)
 
         self.assertEqual(output, 400)
+    
     
     def test_delete_file1(self):
         # get name of an excicting file on server.   
         output = server.delete_file("file_on_server.txt")
         
         self.assertEqual(output, ('', 200))
-        
+       
+    
     def test_delete_file2(self):
         # get name of non-excicting file on server.
         output = server.delete_file("file/document/not_a_file_on_server.txt")
